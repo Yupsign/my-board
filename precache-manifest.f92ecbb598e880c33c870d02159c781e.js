9e3cb254ec69e04f03a648c7afb1de0ff2449b78
Yupsign <miguthree9927@gmail.com>
2021-12-28 12:18:17 +0900

    Updates

---
+++ precache-manifest.f92ecbb598e880c33c870d02159c781e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "698a1eb927dbfe0793962956c6b239aa",
+    "url": "/my-board/index.html"
+  },
+  {
+    "revision": "641c92a29384d04ddac4",
+    "url": "/my-board/static/css/2.5aa997f2.chunk.css"
+  },
+  {
+    "revision": "42039ca7ff183f0a79ad",
+    "url": "/my-board/static/css/main.3a4d78ee.chunk.css"
+  },
+  {
+    "revision": "641c92a29384d04ddac4",
+    "url": "/my-board/static/js/2.ced26dd0.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/my-board/static/js/2.ced26dd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42039ca7ff183f0a79ad",
+    "url": "/my-board/static/js/main.d90081a0.chunk.js"
+  },
+  {
+    "revision": "65a3abeb49240157471f",
+    "url": "/my-board/static/js/runtime-main.0bca9adb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1c8c9776c3f91adfac6700a0e28dd5b6.js"
+  "/my-board/precache-manifest.f92ecbb598e880c33c870d02159c781e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-board/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
